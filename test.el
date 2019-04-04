3a52fa8c91f1f4b302aa4304ba26240eb863fc11
Ulas Turkmen <ulas@micropsi-industries.com>
2019-04-04 17:35:41 +0200

    Macros are hard

--- test.el
+++ test.el
@@ -2,16 +2,17 @@
 (require 'f)
 (require 'elisp-venv)
 
-(defmacro with-custom-var(&rest custom-var-definitions)
-  """Run code with temporary customization options"""
-  `(let ((old-values ()))))
+  ;;"""Run code with temporary customization options"""
+
+(defmacro with-custom-var(custom-vars &rest body)
+  `(let ((old-values (mapcar (lambda (x) (list (quote (car x)) (car x))) ,@custom-vars)))
+     (unwind-protect
+	 (progn ,@body))))
 
 (ert-deftest test-dir-path-from-package-name ()
   (should (string-equal (elisp-venv-dir-path-from-package-name "the-package")
 			(f-full "~/.elisp-venv/the-package-master-sandbox")))
-  (setq elisp-venv-use-git-branch nil)
   (should (string-equal (elisp-venv-dir-path-from-package-name "the-package")
-			(f-full "~/.elisp-venv/the-package-sandbox")))
-  )
+			(f-full "~/.elisp-venv/the-package-sandbox"))))
 
 (ert 't)
